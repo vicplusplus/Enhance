049c438d2dc28ca7244d3b5713d4b5a1015e3d8a
PinkJacketKid <sts122000@gmail.com>
2023-09-16 17:04:41 -0400

    Created temporary title scene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,19 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Scene
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void PlayGame()
     {
-        
+        SceneManager.LoadScene("Main");
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    public void QuitGame()
     {
-        
+        Debug.Log("QUIT");
+        Application.Quit();
     }
 }

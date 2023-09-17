e70246f8e7e4e3fae76e40fc461c1a896dba9ef4
Victor Martinez <vicplusplus@proton.me>
2023-09-17 04:33:37 -0400

    fixed, simplified, and extended randomness

--- Assets/Scripts/ItemSpawner.cs
+++ Assets/Scripts/ItemSpawner.cs
@@ -6,18 +6,46 @@ using UnityEngine.InputSystem;
 public class ItemSpawner : MonoBehaviour
 {
     // Can randomly pick an item to spawn for more variety
-    public List<GameObject> itemPrefabs;
-    public int rarityExponent;
+    public List<WeightedObject> itemPrefabs;
     public GameObject currentObject;
+
+    private float totalWeights;
+
+    private void Start()
+    {
+        totalWeights = 0;
+        foreach (WeightedObject o in itemPrefabs)
+        {
+            totalWeights += o.weight;
+        }
+        print(totalWeights);
+    }
 
     public void SpawnItem()
     {
         if (!currentObject)
         {
-            GameObject selectedPrefab = itemPrefabs[(int)(Mathf.Pow(Random.value, rarityExponent) * (itemPrefabs.Count))];
+            GameObject selectedPrefab = null;
+            float rand = Random.value;
+            foreach (WeightedObject o in itemPrefabs)
+            {
+                if(rand < o.weight / totalWeights)
+                {
+                    selectedPrefab = o.obj;
+                    break;
+                }
+                rand -= o.weight / totalWeights;
+            }
             // Accounting for fall animation
             Vector3 pos = selectedPrefab.GetComponent<Item>().defaultPosition + 2.78f * Vector3.up;
             currentObject = Instantiate(selectedPrefab, pos, Quaternion.identity, transform);
         }
+    }
+
+    [System.Serializable]
+    public struct WeightedObject
+    {
+        public GameObject obj;
+        public float weight;
     }
 }
